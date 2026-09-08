2c1bba36928479d97e16b2a3f0f7598105d7d22c
HiddenCirno <2301697863@qq.com>
2026-09-08 16:09:52 +0800

    单局条件修复

--- Core.cs
+++ Core.cs
@@ -29,7 +29,7 @@ public record ModMetadata : IModMetadata
 
     public List<string>? Contributors { get; init; }
 
-    public SemanticVersioning.Version Version { get; init; } = new("1.4.5");
+    public SemanticVersioning.Version Version { get; init; } = new("1.4.6");
 
     public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.0");
 
