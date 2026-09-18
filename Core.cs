4dca6cecaa0721546541783d8a405397740fbcee
HiddenCirno <2301697863@qq.com>
2026-09-18 13:01:09 +0800

    任务物品刷新修复

--- Core.cs
+++ Core.cs
@@ -29,7 +29,7 @@ public record ModMetadata : IModMetadata
 
     public List<string>? Contributors { get; init; }
 
-    public SemanticVersioning.Version Version { get; init; } = new("1.5.0");
+    public SemanticVersioning.Version Version { get; init; } = new("1.5.1");
 
     public SemanticVersioning.Range SptVersion { get; init; } = new("~4.1.0");
 
